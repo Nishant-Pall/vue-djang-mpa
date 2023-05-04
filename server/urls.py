57101509620885ce8b81c12abf09aeefe30f11f9
Nishant Pall <palln6935@gmail.com>
2023-05-04 11:41:09 +0530

    Setup rest_framework

--- server/urls.py
+++ server/urls.py
@@ -17,4 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+]
