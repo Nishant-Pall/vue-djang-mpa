ff87467d8748726ff4c29c59920e8740c743f21b
Nishant Pall <palln6935@gmail.com>
2023-05-04 14:54:54 +0530

    Add axios to client

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { axios } from "vue/types/umd";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
 
 new Vue({
 	render: (h) => h(App),
 	router,
+	axios,
+	VueAxios,
 }).$mount("#app");
