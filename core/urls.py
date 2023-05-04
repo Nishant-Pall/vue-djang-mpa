b9f8163c7b4032466c7cebb096c08f11dd6298d1
Nishant Pall <palln6935@gmail.com>
2023-05-04 14:42:08 +0530

    Record game scores for each user

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import HomeView, MathGameView, AnagramGameView
+from core.views import HomeView, MathGameView, AnagramGameView, record_score
 
 app_name = "core"
 urlpatterns = [
     path("", HomeView.as_view(), name="homepage"),
     path("math-game/", MathGameView.as_view(), name="math-game"),
-    path("anagram-game", AnagramGameView.as_view(), name="anagram-game"),
+    path("anagram-game/", AnagramGameView.as_view(), name="anagram-game"),
+    path("record-score/", record_score, name="record-score"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,20 @@
+import json
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+from core.models import GameScore
+
+
+def record_score(request):
+    data = json.loads(request.body)
+
+    user_name = data["user-name"]
+    game = data["game"]
+    score = data["score"]
+
+    new_score = GameScore(user_name=user_name, game=game, score=score)
+
+    return JsonResponse({"success": True})
 
 
 class HomeView(TemplateView):
